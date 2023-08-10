4dc96a0c28953180a3d8dce0544a4da30ba2e482
JinweiOS <jinwei.peng@beingthink.com>
2023-08-10 10:01:57 +0800

    feat: 时间传递

--- src/Main.vue
+++ src/Main.vue
@@ -3,36 +3,48 @@ import PartComp from '@/component/Part.vue'
 import AttrComp from '@/component/Attr.vue'
 
 import TableComp from '@/component/Table.vue'
-import {ref} from 'vue'
+import CustomInput from '@/component/CustomInput.vue'
+import { ref } from 'vue'
 export default {
   name: 'MainComp',
   // 局部注册
   components: {
     // 'PartCom': PartComp
     AttrComp,
-    TableComp
+    TableComp,
+    CustomInput
   },
   setup() {
     const msgText = ref('hello world')
     const msgObj = ref({
       name: '哈哈哈哈'
     })
-    setTimeout(() => {
-      msgObj.value.name = '我改变了'
-    }, 3000)
+
 
     const count = ref(0)
+    const customInput = ref('0941')
 
     function add(a, b, c) {
       count.value = a + b
       msgObj.value.name = c
     }
 
+    function getData(data) {
+      console.log('获取数据', data)
+    }
+
+
+    setTimeout(() => {
+      msgObj.value.name = '我改变了'
+      customInput.value = '0943'
+    }, 3000)
     return {
       msgText,
       msgObj,
       add,
-      count
+      count,
+      customInput,
+      getData
     }
   }
 }
@@ -44,7 +56,12 @@ export default {
   <div>父亲：{{ msgObj.name }}</div>
   <div>父亲:{{ count }}</div>
   <el-button>这是elementui提供的</el-button> -->
-  <table-comp></table-comp>
+  <custom-input v-model="customInput"></custom-input>
+  <!-- <CustomInput
+    :modelValue="customInput"
+    @update:modelValue="(newValue) => (customInput = newValue)"
+  /> -->
+  <table-comp @submit="getData"></table-comp>
   <attr-comp class="title-pjw" @click="() => console.log('p')"></attr-comp>
 </template>
 
@@ -53,4 +70,3 @@ export default {
   color: red !important;
 }
 </style>
-
--- src/component/Table.vue
+++ src/component/Table.vue
@@ -1,8 +1,9 @@
 <script>
 import { ref } from 'vue'
 export default {
+  emits: ['submit'],
   name: 'TableComp',
-  setup() {
+  setup(props, { emit }) {
     const tableData = ref([
       {
         date: '2016-05-03',
@@ -30,9 +31,15 @@ export default {
       const time = new Date(date)
       return `${time.getFullYear()}年${time.getMonth() + 1}月${time.getDate()}日`
     }
+
+    function callback() {
+      emit('submit', {data: tableData.value})
+    }
+
     return {
       tableData,
-      print
+      print,
+      callback
     }
   }
 }
@@ -40,12 +47,14 @@ export default {
 
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" >
-      <template v-slot:default="{row}">
+    <el-table-column prop="date" label="Date" width="180">
+      <template v-slot:default="{ row }">
         {{ print(row.date) }}
       </template>
     </el-table-column>
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
   </el-table>
+  <el-button @click="callback">表格数据传递至父级</el-button>
+  <!-- <el-button @click="$emit('submit', tableData)">表格数据传递至父级</el-button> -->
 </template>
